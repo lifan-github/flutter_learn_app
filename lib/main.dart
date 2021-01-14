f02f22c377e4f40e7a44be5b9b56370e0d88a77c
lf73051 <18266913301@163.com>
2021-01-14 14:09:00 +0800

    路由单独文件处理

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learn_app/routes/tab_navigator.dart';
-import 'package:flutter_learn_app/views/assets/details.dart';
-import 'package:flutter_learn_app/views/home/child_page.dart';
-import 'package:flutter_learn_app/views/home/details.dart';
-import 'package:flutter_learn_app/views/home/father_child_page.dart';
-import 'package:flutter_learn_app/views/home/flutter_localizations_page.dart';
-import 'package:flutter_learn_app/views/home/gridView.dart';
-import 'package:flutter_learn_app/views/home/iconfont_page.dart';
-import 'package:flutter_learn_app/views/home/listTile.dart';
-import 'package:flutter_learn_app/views/home/listview.dart';
-import 'package:flutter_learn_app/views/home/listview_load_more_page.dart';
-import 'package:flutter_learn_app/views/home/local_cache_page.dart';
-import 'package:flutter_learn_app/views/home/network.dart';
+import 'package:flutter_learn_app/routes/routes.dart';
 import 'package:flutter_learn_app/views/login/login.dart';
 import 'package:flutter_learn_app/views/noRoute/no_route.dart';
-import 'package:flutter_learn_app/views/user/details.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 import 'model/home_model.dart';
@@ -35,34 +21,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    // 默认语言监测 注意监测null
-    // Locale myLocale = Localizations.localeOf(context); // null || en_US
-    // print('--------------->$myLocale');
-    // String lang = 'en';
-    // String countries = 'US';
-    // if(myLocale != null){
-    //   lang = myLocale.toString().split("_")[0];
-    //   countries = myLocale.toString().split("_")[1];
-    // } else {
-    //   // 不存在默认取系统语言
-    //   lang = 'zh';
-    //   countries = 'CN';
-    // }
-
     return MaterialApp(
-      localizationsDelegates: [  // 国际化语言配置
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('en', 'US'), // 美国英语
-        const Locale('zh', 'CN'), // 中文简体
-      ],
-      // locale: Locale(lang, countries),
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -71,22 +33,7 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: (setting){
         print('路由钩子---$setting');
         final isLogin = true;
-        final routes = {
-          "/": (context) => Tabnavigator(),
-          "home_details": (context) => HomeDetails(),
-          "network_page": (context) => NetworkPage(),
-          "listview_page": (context) => ListViewPage(),
-          "gridview_page": (context) => GridViewPage(),
-          "assets_details": (context) => AssetsDetails(),
-          "user_details": (context) => UserDetails(),
-          "local_cache_page": (context) => LocalCachePage(),
-          "listview_load_more_page": (context) => ListViewLoadMorePage(),
-          "listtile_page": (context) => ListTilePage(),
-          "iconfont_page": (context) => IconFontPage(),
-          "loaclizations_page": (context) => LocalizationPage(),
-          "father_child_page": (context) => FatherChildPage(),
-          "child_page": (context) => ChildPage(),
-        };
+        final routes = RoutesConfig.routes;
         if(!isLogin){
           return MaterialPageRoute(builder: (context) => LoginRoute());
         }
--- lib/routes/tab_navigator.dart
+++ lib/routes/tab_navigator.dart
@@ -10,35 +10,38 @@ class Tabnavigator extends StatefulWidget {
 
 class _TabnavigatorState extends State<Tabnavigator> {
   int _selectedIndex = 0;
-  final PageController _controller = PageController(initialPage: 0, keepPage: true);
+  final PageController _controller =
+      PageController(initialPage: 0, keepPage: true);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        controller: _controller,
-        onPageChanged: (int index){
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        children: <Widget>[
-          HomePage(),
-          AssetsPage(),
-          UserPage(),
-        ]
-      ),
+          controller: _controller,
+          onPageChanged: (int index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          children: <Widget>[
+            HomePage(),
+            AssetsPage(),
+            UserPage(),
+          ]),
       bottomNavigationBar: BottomNavigationBar(
-        // 底部导航
         items: <BottomNavigationBarItem>[
           // ignore: deprecated_member_use
           BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
           BottomNavigationBarItem(
               // ignore: deprecated_member_use
-              icon: Icon(Icons.business), title: Text('Business')),
+              icon: Icon(Icons.business),
+              // ignore: deprecated_member_use
+              title: Text('Business')),
           BottomNavigationBarItem(
               // ignore: deprecated_member_use
-              icon: Icon(Icons.school), title: Text('School')),
+              icon: Icon(Icons.school),
+              // ignore: deprecated_member_use
+              title: Text('School')),
         ],
         currentIndex: _selectedIndex,
         fixedColor: Colors.blue,
